6cb102c4f3d97f27bfdd6005c8356c3974ba16a7
Lee Jun Kit <mohawk.riceball@gmail.com>
2016-05-06 16:00:13 +0800

    compile and digest to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
 
-  config.assets.compile = false
-  config.assets.digest = false
+  config.assets.compile = true
+  config.assets.digest = true
   config.log_level = :debug
   config.i18n.fallbacks = true
 
